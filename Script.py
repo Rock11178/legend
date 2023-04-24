5225a76c3e981156fc1b33cf0d09b42b7d0e773d
Rock11178 <128242010+Rock11178@users.noreply.github.com>
2023-04-24 17:56:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -302,8 +302,8 @@ My Nᴀᴍᴇ Iꜱ <a href=https://t.me/{}>{}</a>, I ᴄᴀɴ Pʀᴏᴠɪᴅᴇ 
     CUSTOM_FILE_CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : <code>{file_name}</code>
 
 ╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
-➪<a href=https://t.me/AECR_MOVIES> AECR MOVIES</a>
-➪<a href=https://t.me/THE_AECR> Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ​ </a>
+➪<a href=https://t.me/Moviezs_Store> AECR MOVIES</a>
+➪<a href=https://t.me/TamilMovies_Workshop> Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ​ </a>
 ╚════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝</b>"""
 
     RESTART_TXT = """
